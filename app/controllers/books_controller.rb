8b1a35bba83e593db2d0d1e8517cb07810eb20ca
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-06-13 12:44:39 +0000

    [update]6/13

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render "index"
     end
   end
 
