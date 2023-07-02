03328998f778d93a309a837e5ca0517403e971c8
momo5QvQ <132321554+momo5QvQ@users.noreply.github.com>
2023-07-02 12:03:30 +0000

    [fin]oyo5

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,20 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+# 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")#完全一致
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")#前方一致
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")#後方一致
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")#部分一致
+    else
+      @user = User.all
+    end
+  end
+
 end
